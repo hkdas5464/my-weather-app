f043797d4a1968a606f19e5bf9b6dd134f4b004c
Harendra Kumar Das <63789292+hkdas5464@users.noreply.github.com>
2020-10-02 02:34:38 +0530

    Updates

---
+++ precache-manifest.729a1640d3a60c7da6c4d7b42d8fe3c8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4a2abedc3321600b687b9bb4a7a671",
+    "url": "/my-weather-app/index.html"
+  },
+  {
+    "revision": "8511b053ff6d27ffef9a",
+    "url": "/my-weather-app/static/css/main.dc374b13.chunk.css"
+  },
+  {
+    "revision": "22ec2739ace836499291",
+    "url": "/my-weather-app/static/js/2.06e3cc75.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/my-weather-app/static/js/2.06e3cc75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8511b053ff6d27ffef9a",
+    "url": "/my-weather-app/static/js/main.5f91438b.chunk.js"
+  },
+  {
+    "revision": "6fec3c0b5463c40690aa",
+    "url": "/my-weather-app/static/js/runtime-main.1a0f41f6.js"
+  },
+  {
+    "revision": "043e75b0b419365847814daf25b103ea",
+    "url": "/my-weather-app/static/media/cloud.043e75b0.png"
+  },
+  {
+    "revision": "3060caa1460bf6e2d125688800589f94",
+    "url": "/my-weather-app/static/media/cloud.3060caa1.jpg"
+  },
+  {
+    "revision": "7923a34c83b8f792b499370f94a5cf04",
+    "url": "/my-weather-app/static/media/moon.7923a34c.png"
+  }
+]);
